a24822e830ca4d270b204a06725c3d5330ddc71a
Aleh Kot <kot.oleg@gmail.com>
2014-06-09 11:10:00 +0300

    Add PHP dependency.

--- manifests/init.pp
+++ manifests/init.pp
@@ -14,19 +14,25 @@
 # === Requirements
 #
 # This class requires the apache class from PuppetLabs.
-class apcu($version = '4.0.4') {  
+class apcu($version = '4.0.4') {
 
   ensure_packages( ['libpcre3-dev'] )
-  
+
   php::pecl::module { "apcu-${version}":
     use_package         => false,
     service_autorestart => true,
     preferred_state     => 'beta',
+    require             => [
+      Class['php'],
+    ],
   }
-  
-  php::augeas { 'augeas-apcu':    
+
+  php::augeas { 'augeas-apcu':
     entry  => "PHP/extension[. = \"apcu.so\"]",
     value  => 'apcu.so',
     notify  => Service['httpd'],
-  }  
+    require             => [
+      Class['php'],
+    ],
+  }
 }
